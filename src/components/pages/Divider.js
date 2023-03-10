6dbb327c782d56ee5a369402fec479cbee9023a5
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-03-09 20:45:51 -0800

    Create Divider.js

---
+++ src/components/pages/Divider.js
@@ -0,0 +1,15 @@
+import react from "react";
+import { StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 20
+    }
+})
+
+const Divider = () => {
+    return(
+        <View style={styles.container}></View>
+    )
+}
+export default Divider;
