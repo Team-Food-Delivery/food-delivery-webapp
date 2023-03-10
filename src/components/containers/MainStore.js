e222e345214ea74995c78b4149e3edf477cb0f51
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-03-09 20:45:49 -0800

    Create MainStore.js

---
+++ src/components/containers/MainStore.js
@@ -0,0 +1,29 @@
+import react from "react";
+import { StyleSheet, View } from "react-native";
+import Carousel from "../pages/Carousel";
+import Divider from "../pages/Divider";
+import CategoriesStyles from "../../styles/Categories.styles";
+import BurgerIcon from '../../img/burger-outline.png'
+import PizzaSliceIcon from '../../img/pizza-slice-outline.png'
+import TurkeyIcon from '../../img/christmas-dinner-outline.png'
+
+const results = [{"id": 1, "store": "Kennedy-Henderson", "phone_number": "(895)291-0253x051", "dish": "Pizza", "dish_description": "Three egg whites with spinach, mushrooms, caramelized onions, tomatoes and low-fat feta cheese. With herbed quinoa, and your choice of rye or whole-grain toast.", "price": 15.46, "rating": 5}, {"id": 2, "store": "Clayton Group", "phone_number": "+1-050-022-5883", "dish": "Pasta and Beans", "dish_description": "Breaded fried chicken with waffles, and a side of maple syrup.", "price": 13.94, "rating": 1}, {"id": 3, "store": "Gillespie-Kennedy", "phone_number": "(853)263-7908x93351", "dish": "Souvlaki", "dish_description": "Fresh parsley, Italian sausage, shallots, garlic, sun-dried tomatoes and mozzarella cheese in an all-butter crust. With a side of mixed fruits.", "price": 16.77, "rating": 5}, {"id": 4, "store": "Brown Ltd", "phone_number": "+1-871-280-9791x12614", "dish": "Tiramis\u00f9", "dish_description": "Three egg omelet with Roquefort cheese, chives, and ham. With a side of roasted potatoes, and your choice of toast or croissant.", "price": 7.88, "rating": 1}, {"id": 5, "store": "Hall-Scott", "phone_number": "479-544-6237", "dish": "Risotto with seafood", "dish_description": "28-day aged 300g USDA Certified Prime Ribeye, rosemary-thyme garlic butter, with choice of two sides.", "price": 6.15, "rating": 4}]
+const images = [{id: 1, source: BurgerIcon}, {id: 2, source: PizzaSliceIcon}, {id:3, source: TurkeyIcon}]
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
+const MainStore = () => {
+    return(
+        <View style={styles.container}>
+            <Divider />
+            <Carousel title='Categories' customStyle={CategoriesStyles} data={images}> </Carousel>
+            <Divider />
+            <Carousel title='Stores' customStyle={CategoriesStyles} data={results}> </Carousel>
+        </View>
+
+    )
+}
+export default MainStore;
