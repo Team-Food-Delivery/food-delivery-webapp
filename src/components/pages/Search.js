93b7f3ab049695d0f2a8d0e80791afe3682a037e
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-03-10 23:01:11 -0800

    Create Search.js

---
+++ src/components/pages/Search.js
@@ -0,0 +1,44 @@
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    title: {
+        alignSelf:'center',
+        fontSize: 18,
+        fontFamily: 'Arial',
+        color: 'gray',
+        marginBottom: 10
+    },
+    bold: {
+        fontWeight: 'bold'
+    },
+    searchBackground: {
+
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        marginHorizontal: 30,
+        borderRadius: 40,
+        backgroundColor: '#fff'
+    }
+});
+
+const Search = ({storeSearch, onStoreSearchChange}) => {
+    
+    return (
+        <View>
+            <Text style={styles.title}>
+                Choose the {<Text style={[styles.title, styles.bold]}>Food you love</Text>}
+            </Text>
+            <View style={styles.searchBackground}>
+                <TextInput
+                    placeholder="Search for a food item"
+                    value={storeSearch}
+                    onChangeText={newStoreSearch => onStoreSearchChange(newStoreSearch)}
+                    onEndEditing={() => console.log('TODO: Submit a request to API')}
+                >
+                </TextInput>
+            </View>
+        </View>
+    )
+};
+
+export default Search;
