c5ff4c7c92a45447002e87849ae9cb9060d3f357
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-03-10 21:13:21 -0800

    View to SafeAreaView

--- src/components/containers/MainStore.js
+++ src/components/containers/MainStore.js
@@ -1,5 +1,5 @@
 import react from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import Carousel from "../pages/Carousel";
 import Divider from "../pages/Divider";
 import CategoriesStyles from "../../styles/Categories.styles";
@@ -10,6 +10,8 @@ import SupremePizzaIcon from '../../img/pizza_2.png'
 import PepperoniPizzaIcon from '../../img/pizza_1.png'
 import PizzaSliceIcon from '../../img/pizza-slice-outline.png'
 import TurkeyIcon from '../../img/christmas-dinner-outline.png'
+import BlackStarIcon from '../../img/star-black.png'
+import WhiteStarIcon from '../../img/star-white.png'
 
 // Fake data with static images, both should come from server
 const results = [{"id": 1, "source":PepperoniPizzaIcon, "store": "Kennedy-Henderson", "phone_number": "(895)291-0253x051", "dish": "Pizza", "dish_description": "Three egg whites with spinach, mushrooms, caramelized onions, tomatoes and low-fat feta cheese. With herbed quinoa, and your choice of rye or whole-grain toast.", "price": 15.46, "rating": 5}, {"id": 2, "source":SupremePizzaIcon,"store": "Clayton Group", "phone_number": "+1-050-022-5883", "dish": "Pasta and Beans", "dish_description": "Breaded fried chicken with waffles, and a side of maple syrup.", "price": 13.94, "rating": 1}, {"id": 3, "source":BurgerFriesIcon, "store": "Gillespie-Kennedy", "phone_number": "(853)263-7908x93351", "dish": "Souvlaki", "dish_description": "Fresh parsley, Italian sausage, shallots, garlic, sun-dried tomatoes and mozzarella cheese in an all-butter crust. With a side of mixed fruits.", "price": 16.77, "rating": 5}, {"id": 4, "source":PepperoniPizzaIcon, "store": "Brown Ltd", "phone_number": "+1-871-280-9791x12614", "dish": "Tiramis\u00f9", "dish_description": "Three egg omelet with Roquefort cheese, chives, and ham. With a side of roasted potatoes, and your choice of toast or croissant.", "price": 7.88, "rating": 1}, {"id": 5, "source":BurgerFriesIcon, "store": "Hall-Scott", "phone_number": "479-544-6237", "dish": "Risotto with seafood", "dish_description": "28-day aged 300g USDA Certified Prime Ribeye, rosemary-thyme garlic butter, with choice of two sides.", "price": 6.15, "rating": 4}]
@@ -22,12 +24,14 @@ const styles = StyleSheet.create({
 })
 const MainStore = () => {
     return(
-        <View style={styles.container}>
-            <Divider />
-            <Carousel title='Categories' customStyle={CategoriesStyles} data={images} />
-            <Divider />
-            <Carousel title='Stores' customStyle={StoreStyles} data={results} />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <Divider />
+                <Carousel title='Categories' customStyle={CategoriesStyles} data={images} />
+                <Divider />
+                <Carousel title='Stores' customStyle={StoreStyles} data={results} />
+            </ScrollView>
+        </SafeAreaView>
 
     )
 }
