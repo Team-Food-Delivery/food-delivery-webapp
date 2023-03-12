44b9cdd51b0067fa57bdfa6a53c65d3eed21081d
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-03-12 11:50:12 -0700

    Update CSS Gradient Styles

--- src/components/pages/Carousel.js
+++ src/components/pages/Carousel.js
@@ -10,7 +10,22 @@ const styles = StyleSheet.create({
         fontFamily: 'Arial',
         color: 'gray',
         fontWeight: 'bold'
+    },
+    shadow: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 3},
+        shadowRadius: 6,
+        shadowOpacity: 0.2,
+        elevation:5
+    },
+    greenShadow: {
+        shadowColor: '#0fde12',
+        shadowOffset: {width: 0, height: 5},
+        shadowRadius: 7,
+        shadowOpacity: 0.6,
+        elevation:5
     }
+
 });
 
 const Carousel = ({title, customStyle, data}) => {
@@ -20,6 +35,33 @@ const Carousel = ({title, customStyle, data}) => {
         pressedId: null
     });
 
+    const checkTitle = (title, item) => {
+        return title === 'Categories' 
+            ? ['transparent'] 
+            : [ '#fff', isPress.pressedId !== item.id ? '#fff': '#0fde12'];
+    }
+
+    const renderItem = (item ) => {
+        return (      
+            <TouchableOpacity
+                style={
+                    isPress.pressedId !== item.id ? styles.shadow : styles.greenShadow
+                }
+                onPress={() => setIsPress({...isPress, pressedId: item.id})}
+            >
+                <LinearGradient
+                    colors={checkTitle(title, item)}
+                    style={customStyle.itemContainer}
+                    end={{x:0.5,y:1.1}}
+
+                >
+                    <Image resizeMode='contain' source={item.source} style={customStyle.item}/>
+                    {item.store && <CarouselDetails item={item} /> }
+                </LinearGradient>
+            </TouchableOpacity>
+        )
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
@@ -28,20 +70,7 @@ const Carousel = ({title, customStyle, data}) => {
                     showsHorizontalScrollIndicator={false}
                     data={data}
                     keyExtractor={(item) => item.id}
-                    renderItem={({ item }) => {
-                        return (
-                                <TouchableOpacity
-                                    style={[customStyle.itemContainer, 
-                                        isPress.pressedId !== item.id ? customStyle.shadow : customStyle.greenShadow
-                                    ]}
-                                    onPress={() => setIsPress({...isPress, pressedId: item.id})}
-                                >
-                                    <Image resizeMode='contain' source={item.source} style={customStyle.item}/>
-                                    {item.store && <CarouselDetails item={item} /> }
-                                </TouchableOpacity>
-
-                        )
-                    }}
+                    renderItem={({item})=>renderItem(item)}
                 />
         </View>
     )
